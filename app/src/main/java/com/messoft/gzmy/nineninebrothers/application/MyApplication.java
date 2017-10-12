b9cf4c87b47f8e4a4dd85766be44c1bb19482644
pengxiansen <17688700269@163.com>
2017-10-12 16:27:26 +0800

    Initial commit

---
+++ app/src/main/java/com/messoft/gzmy/nineninebrothers/application/MyApplication.java
@@ -0,0 +1,22 @@
+package com.messoft.gzmy.nineninebrothers.application;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Administrator on 2017/10/12 0012.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
